fd92ce3fdde23b08e952cb9229b8155fb511d2a9
Victória Gama Carvalho <victoriagama80@gmail.com>
2023-06-06 12:29:43 -0300

    início controle de versão

---
+++ src/main/java/com/comercio/comercio/controller/LojaController.java
@@ -0,0 +1,19 @@
+package com.comercio.comercio.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/comercio/produtos")
+public class LojaController {
+	
+	@GetMapping("/listar")
+	public void listarProdutos() {
+		
+		System.out.println("LISTA DE PRODUTOS!");{
+		
+		return "produtos";
+
+}
+} 
---
+++ src/main/java/com/comercio/comercio/model/Produto.java
@@ -0,0 +1,19 @@
+package com.comercio.comercio.model;
+
+public class Produto {
+
+	private Long id;
+	private String nome;
+	private String descrição;
+	private String codigoBarras;
+	private double preco;
+	
+	// public: Acesso liberado para todas as classes
+	
+	// private: Acesso permitido apenas para os membros 
+	//          da própria classe.
+	//          Entende-se por membros(atributos e ou métodos)
+
+	}
+
+
