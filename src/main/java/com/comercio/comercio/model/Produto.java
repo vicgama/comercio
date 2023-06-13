f6eef5440ec7cac227e6c28c1470abd5390523ed
Victória Gama Carvalho <victoriagama80@gmail.com>
2023-06-13 12:31:38 -0300

    Ajustes nos arquivos LojaController e Produto

--- src/main/java/com/comercio/comercio/model/Produto.java
+++ src/main/java/com/comercio/comercio/model/Produto.java
@@ -13,6 +13,42 @@ public class Produto {
 	// private: Acesso permitido apenas para os membros 
 	//          da própria classe.
 	//          Entende-se por membros(atributos e ou métodos)
+	//Métodos Setter's e Getter's : atribuuir e recuperar dados do atributo "RESPECTIVAMENTE"
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setDescrição(String descrição) {
+		this.descrição = descrição;
+	}
+	public String getDescrição() {
+		return descrição;
+	}
+	public void setCodigoBarras(String codigoBarras) {
+		this.codigoBarras = codigoBarras;
+	}
+	public String getCodigoBarras() {
+		return codigoBarras;
+	}
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+	public double getPreco() {
+		return preco;
+	}
+
+
+
+
 
 	}
 
