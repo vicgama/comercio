943551ed27a05487f517acc46cbf7f4b3ef550d2
Victória Gama Carvalho <victoriagama80@gmail.com>
2023-06-19 08:03:32 -0300

    Editando o template produtos

--- src/main/java/com/comercio/comercio/controller/LojaController.java
+++ src/main/java/com/comercio/comercio/controller/LojaController.java
@@ -1,5 +1,8 @@
 package com.comercio.comercio.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +12,8 @@ import com.comercio.comercio.model.Produto;
 @Controller
 @RequestMapping("/comercio/produtos")
 public class LojaController {
+	
+	List<Produto> ListaDeProdutos = new ArrayList<Produto>();
 	
 	@GetMapping("/listar")
 	public String listarProdutos() {
@@ -26,6 +31,17 @@ public class LojaController {
 		p2.setCodigoBarras("99887VHVHVHHPP74");
 		p2.setDescrição("Com 350W de potência e 4 velocidades, diferentes regulagens para melhor se adequar aos mais variados tipos de receitas.");
 		p2.setPreco(250.00);
+		
+		Produto p3 = new Produto();
+		p3.setId(20l);
+		p3.setNome("Televisor 70'");
+		p3.setCodigoBarras("1245GFGVPCOUFTDH");
+		p3.setDescrição("Televisor com tela plana let Samsung");
+		p3.setPreco(6235.70);
+		
+		// Adicionando os produtos à lista
+		
+		ListaDeProdutos.add(p1);
 		
 		return "produtos";
 		}
